af41cc55379e13fc0e3cd3b16ba469218b117dce
Oliver Cook <olliewatford@gmail.com>
2024-01-25 21:51:36 +0000

    Finished

--- app/components/Input/Input.tsx
+++ app/components/Input/Input.tsx
@@ -1,10 +1,16 @@
 "use client"
 
 import { useState } from "react"
+import Lottie from "lottie-react"
+import animationData from "@/public/Animation - 1706218481745.json"
 
 export default function Input() {
   const [time, setTime] = useState(0);
   const [timer, setTimer] = useState<NodeJS.Timeout | null>(null);
+  const [inputValue, setInputValue] = useState("");
+  const [error, setError] = useState<string | null>(null);
+  const [success, setSuccess] = useState(false);
+  const [loop, setLoop] = useState(true);
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "c") {
@@ -19,15 +25,85 @@ export default function Input() {
     if (event.key === "g") {
       if (timer) {
         clearInterval(timer);
+        if (inputValue === "console.lo") {
+          setSuccess(true);
+        } else {
+          setError("Wrong input");
+        }
       }
     }
   }
 
+  const handleStartOver = () => {
+    setTime(0)
+    setInputValue("")
+    setError(null)
+    setSuccess(false)
+  }
+
   return (
-    <div className="flex flex-col mt-16">
-      <label>Type console.log as fast as you can</label>
-      <input type="text" className="border-2 border-black indent-2" onKeyDown={handleKeyDown} />
-      <p>time: {time}</p>
+    <div className="flex flex-col items-center mt-16">
+      <label htmlFor="console-log-type" className="mb-2 font-semibold">Type 'console.log' as fast as you can</label>
+      <div className={`flex relative left-9`}>
+        <input 
+          id="console-log-type"
+          type="text" 
+          className="border-4 border-black indent-1 h-16 w-96 text-4xl font-semibold" 
+          onKeyDown={handleKeyDown}
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
+        />
+        <button 
+          className="w-16 ml-2 aspect-square bg-orange-400 border-4 border-black text-white font-bold hover:bg-orange-500"
+          onClick={handleStartOver}
+        >
+          Clear
+        </button>
+      </div>
+      <p className="h-6 text-red-800 font-bold">{error}</p>
+      <p className="mt-2">TIMER</p>
+      <p className={`text-6xl font-bold font-mono ${success == true ? 'text-lime-500' : 'text-teal-200'}`}>{time.toFixed(2)}</p>
+      {
+        success == true && (
+          <>
+          <div className="relative mt-20 h-32 w-[40rem] bg-orange-100 rounded-lg flex flex-col items-center justify-center">
+            <p className="text-2xl font-semibold">Congratulations!</p>
+            <p>You typed 'console.log' in {time} seconds!</p>
+            <p>What an achievement!</p>
+            <Lottie
+              animationData={animationData}
+              className="flex justify-center items-center h-32 absolute left-0"
+              loop={loop}
+              style={{ transform: 'scaleX(-1)' }}
+            />
+            <Lottie
+              animationData={animationData}
+              className="flex justify-center items-center h-32 absolute right-0"
+              loop={loop}
+            />
+          </div>
+          {
+            loop === true ? (
+              <button 
+                className="absolute bottom-2 border-2 border-black p-2 bg-red-400 hover:bg-red-500"
+                onClick={() => setLoop(false)}
+              >
+                Stop party poppers
+              </button>
+            )
+            :
+            (
+              <button 
+                className="absolute bottom-2 border-2 border-black p-2 bg-green-400 hover:bg-green-500"
+                onClick={() => setLoop(true)}
+              >
+                Start party poppers
+              </button>
+            )
+          }
+          </>
+        )
+      }
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Input from "./components/Input"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
-      <h1 className="mt-16">console.log speed type test</h1>
+      <h1 className="mt-16 text-6xl font-bold text-white">console.log speed type test</h1>
       <Input />
     </main>
   );
