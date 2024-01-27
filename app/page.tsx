68413c63a160f582d3f4591b57a4588af0b74d4b
Oliver Cook <olliewatford@gmail.com>
2024-01-27 16:19:39 +0000

    safari improvement

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Input from "./components/Input"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
+    <main className="flex min-h-[100svh] flex-col items-center">
       <h1 className="mt-16 text-6xl font-bold text-white text-center">console.log speed type test</h1>
       <Input />
     </main>
